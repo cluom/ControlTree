d7252d4180f216429d7241109bccaf0ebeaaa873
cluom <c_l-m@qq.com>
2024-05-03 04:40:30 +0800

    增加对手持数量的判断

--- BetterDeconstructor/Patches/ObjectPatch.cs
+++ BetterDeconstructor/Patches/ObjectPatch.cs
@@ -31,18 +31,20 @@ public class ObjectPatch
 
         var array1 = str.Split('/');
         if (ArgUtility.SplitBySpace(ArgUtility.Get(array1, 2)).Length <= 1) return;
+
+        var inputCount = int.Parse(ArgUtility.SplitBySpace(ArgUtility.Get(array1, 2))[1]);
+        if (inputItem.Stack < inputCount) return;
+
         SObject? object1 = null;
         string[] array2 = ArgUtility.SplitBySpace(ArgUtility.Get(array1, 0));
-        var inputCount = int.Parse(ArgUtility.SplitBySpace(ArgUtility.Get(array1, 2))[1]);
         for (var index = 0; index < array2.Length; index += 2)
         {
             var object2 = new SObject(ArgUtility.Get(array2, index), ArgUtility.GetInt(array2, index + 1, 1));
             if (
-                object1 != null && 
+                object1 != null &&
                 object2.sellToStorePrice(-1L) * object2.Stack <= object1.sellToStorePrice() * object1.Stack
             ) continue;
             object1 = object2;
-
         }
 
         __result = object1;
