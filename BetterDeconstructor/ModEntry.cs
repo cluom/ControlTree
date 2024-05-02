a50ec8165b8f2bad237dec0c944de7dce13e575c
cluom <c_l-m@qq.com>
2024-05-03 04:25:33 +0800

    更好的分解机V1.0.0

---
+++ BetterDeconstructor/ModEntry.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+
+namespace BetterDeconstructor;
+
+// ReSharper disable once UnusedType.Global
+internal class ModEntry : Mod
+{
+    // ReSharper disable once NotAccessedField.Global
+    public static ModEntry? Instance;
+
+    public override void Entry(IModHelper helper)
+    {
+        Instance = this;
+
+        // 启用Harmony补丁
+        Harmony harmony = new(ModManifest.UniqueID);
+        harmony.PatchAll();
+    }
+}
---
+++ BetterDeconstructor/Patches/ObjectPatch.cs
@@ -0,0 +1,52 @@
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Extensions;
+using StardewValley.GameData.Machines;
+using SObject = StardewValley.Object;
+
+namespace BetterDeconstructor.Patches;
+
+[Harmony]
+// ReSharper disable once UnusedType.Global
+public class ObjectPatch
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(SObject), "OutputDeconstructor")]
+    public static void Postfix_OutputDeconstructor(
+        SObject __instance,
+        SObject machine,
+        Item inputItem,
+        bool probe,
+        MachineItemOutput outputData,
+        ref Item? __result)
+    {
+        if (!inputItem.HasTypeObject() && !inputItem.HasTypeBigCraftable())
+        {
+            return;
+        }
+
+        if (!CraftingRecipe.craftingRecipes.TryGetValue(inputItem.Name, out var str))
+        {
+            return;
+        }
+
+        var array1 = str.Split('/');
+        if (ArgUtility.SplitBySpace(ArgUtility.Get(array1, 2)).Length <= 1) return;
+        SObject? object1 = null;
+        string[] array2 = ArgUtility.SplitBySpace(ArgUtility.Get(array1, 0));
+        var inputCount = int.Parse(ArgUtility.SplitBySpace(ArgUtility.Get(array1, 2))[1]);
+        for (var index = 0; index < array2.Length; index += 2)
+        {
+            var object2 = new SObject(ArgUtility.Get(array2, index), ArgUtility.GetInt(array2, index + 1, 1));
+            if (
+                object1 != null && 
+                object2.sellToStorePrice(-1L) * object2.Stack <= object1.sellToStorePrice() * object1.Stack
+            ) continue;
+            object1 = object2;
+
+        }
+
+        __result = object1;
+        if (object1 is null) return;
+        inputItem.Stack -= inputCount + 1;
+    }
+}
